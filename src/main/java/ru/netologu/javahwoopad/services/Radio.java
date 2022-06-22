926cf9cd74eb76ec01132b474d42bd11559183a4
Petr <petr_mameev@mail.ru>
2022-06-22 22:55:53 +0700

    fourth commit

--- src/main/java/ru/netologu/javahwoopad/services/Radio.java
+++ src/main/java/ru/netologu/javahwoopad/services/Radio.java
@@ -1,2 +1,81 @@
+package ru.netologu.javahwoopad.services;
+
 public class Radio {
+    public int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 10;
+    }
+
+    public void setToMinVolume() {
+        currentVolume = 0;
+    }
+
+    public int currentWave;
+
+    public int getCurrentWave() {
+        return currentWave;
+    }
+
+    public void nextWave() {
+        if (currentWave < 9) {
+            currentWave = currentWave + 1;
+        }
+        if (currentWave == 9) {
+            currentWave = 0;
+        }
+    }
+
+    public void prevWave() {
+        if (currentWave > 0) {
+            currentWave = currentWave - 1;
+        }
+        if (currentWave == 0) {
+            currentWave = 9;
+        }
+    }
+
+    public void setCurrentWave(int newCurrentWave) {
+        if (newCurrentWave > 9) {
+            return;
+        }
+        if (newCurrentWave < 0) {
+            return;
+        }
+        currentWave = newCurrentWave;
+    }
+
+    public void setToMaxWave() {
+        currentWave = 9;
+    }
+
+    public void setToMinWave() {
+        currentWave = 0;
+    }
 }
--- src/test/java/ru/netologu/javahwoopad/services/RadioTest.java
+++ src/test/java/ru/netologu/javahwoopad/services/RadioTest.java
@@ -1,2 +1,157 @@
-package PACKAGE_NAME;public class RadioTest {
+package ru.netologu.javahwoopad.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void shouldSetVolume() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(5);
+        int expected = 5;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetToMaxVolume() {
+        Radio rad = new Radio();
+        rad.setToMaxVolume();
+        int expected = 10;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMinVolume() {
+        Radio rad = new Radio();
+        rad.setToMinVolume();
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeAboveMax() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(20);
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeBelowMin() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(-5);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolume() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(5);
+        rad.increaseVolume();
+        int expected = 6;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReduceVolume() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(5);
+        rad.reduceVolume();
+        int expected = 4;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetWave() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(5);
+        int expected = 5;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldNotSetWaveBelowMin(){
+        Radio rad = new Radio();
+        rad.setCurrentWave(-5);
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetWaveAboveMax() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(20);
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNextWaveAfterMaxWave() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(9);
+        rad.nextWave();
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNextWave() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(5);
+        rad.nextWave();
+        int expected = 6;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shoulSetPrevWave() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(5);
+        rad.prevWave();
+        int expected = 4;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shoulSetPrevAfterMinWave() {
+        Radio rad = new Radio();
+        rad.setCurrentWave(0);
+        rad.prevWave();
+        int expected = 9;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetToMinWave() {
+        Radio rad = new Radio();
+        rad.setToMinWave();
+        int expected = 0;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetToMaxWave() {
+        Radio rad = new Radio();
+        rad.setToMaxWave();
+        int expected = 9;
+        int actual = rad.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
